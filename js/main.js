71f2de6d53112eb685ae56e87ae73679587d740e
Дмитрий Скворцов <nickbox99@inbox.ru>
2020-05-22 04:03:51 +0400

    Add section project and section goals. Adapted all section. Add two sliders and create js code for numbers sider

--- js/main.js
+++ js/main.js
@@ -19,9 +19,8 @@ $(document).ready(function () {
     }
   });
 
-  
   let btn = document.querySelector(".button-up");
-  
+
   function magic() {
     if (window.pageYOffset > 1000) {
       btn.style.opacity = "1";
@@ -37,4 +36,82 @@ $(document).ready(function () {
 
   // When scrolling, we run the function
   window.onscroll = magic;
+
+
+  //====================
+  //===Слайдеры===
+  //====================
+
+  var arrowPrev = $(".swiper-button-prev");
+
+  //====================
+  //===Первый слайдер===
+  //====================
+
+  //initialize swiper when document ready
+  var mySwiper1 = new Swiper(".swiper-container1", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next1",
+      prevEl: ".swiper-button-prev1",
+    },
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination1",
+      clickable: true,
+    },
+  });
+
+  var next = $(".swiper-button-next1");
+  var buttons = $(".swiper-pagination1");
+
+  next.css("left", arrowPrev.width() + 25 + buttons.width() + 30);
+  buttons.css("left", arrowPrev.width() + 25);
+
+  //====================
+  //===Второй слайдер===
+  //====================
+
+  //initialize swiper when document ready
+  var mySwiper2 = new Swiper(".swiper-container2", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next2",
+      prevEl: ".swiper-button-prev2",
+    },
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination2",
+      clickable: true,
+    },
+  });
+  
+  var next2 = $(".swiper-button-next2");
+  var buttons2 = $(".swiper-pagination2");
+
+  next2.css("left", arrowPrev.width() + 25 + buttons2.width() + 30);
+  buttons2.css("left", arrowPrev.width() + 25);
+
+  //======================
+  //===Обработчик чисел===
+  //======================
+
+  $(".swiper-button-prev2, .swiper-button-next2,.swiper-pagination2").on(
+    "click",
+    function () {
+      var number = 0;
+      var children = buttons2.children();
+
+      children.each(function (idx, val) {
+        number++;
+        if($(this).hasClass("swiper-pagination-bullet-active"))
+        {
+          $(".swiper-counter__now").text(number);
+          $(".goals__item").removeClass("goals__item-active");
+          $(".goals__items").children().eq(number-1).addClass("goals__item-active");
+          return false;
+        }
+      });
+    }
+  );
 });
